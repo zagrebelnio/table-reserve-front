967b352b3e9524081643b196f0abd40aaada4a22
Oleksandr <zagrebelniysasha@gmail.com>
2024-11-28 22:16:13 +0200

    fix: render page when auth data is loading

--- src/app/(pages)/profile/layout.js
+++ src/app/(pages)/profile/layout.js
@@ -3,7 +3,9 @@ import { useAuth } from '@/app/lib/auth/authContext';
 import Unauthorized from '@/app/ui/unauthorized';
 
 export default function ProfileLayout({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return null;
 
   return <>{isAuthenticated ? children : <Unauthorized />}</>;
 }
